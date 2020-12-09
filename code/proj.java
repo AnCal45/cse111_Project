d941e92a8755adf0384e5228584c8ed1ecf2a797
AnCal45 <acalvillo511@gmail.com>
2020-12-09 15:23:40 -0800

    added new functions and began working on user interface

--- code/proj.java
+++ code/proj.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.io.File;
+import java.util.Scanner;
 
 public class proj {
     private Connection c = null;
@@ -208,6 +209,9 @@ public class proj {
             sql = "drop table Dimension";
             stmt.execute(sql);
 
+            sql = "drop table FromWhere";
+            stmt.execute(sql);
+
             c.commit();
             
             stmt.close();
@@ -228,11 +232,289 @@ public class proj {
 
     }
 
-    
-    private void queryName(/* insert variables if needed */){
-        //will need at least 20 queries for the project
-        // each query will have it's own function
+
+    //----------------------------DC----------------------------
+    private void allDCChars(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, sh_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Superhero, Race " +
+            "where nl_ID = sh_ID and sh_race = r_ID and sh_from = 2 " +
+            "UNION " +
+            "SELECT nl_Name as realName, v_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Villain, Race " +
+            "where nl_ID = v_ID and v_race = r_ID and v_from = 2 " +
+            "union  " +
+            "SELECT nl_Name as realName, ah_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Antihero, Race " +
+            "where nl_ID = ah_ID and ah_race = r_ID and ah_from = 2;";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
     }
+
+
+    private void allDCHeroes(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, sh_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Superhero, Race " +
+            "where nl_ID = sh_ID and sh_race = r_ID and sh_from = 2";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
+    private void allDCVillains(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, v_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Villain, Race " +
+            "where nl_ID = v_ID and v_race = r_ID and v_from = 2";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
+    private void allDCAntiHeroes(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, ah_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Antihero, Race " +
+            "where nl_ID = ah_ID and ah_race = r_ID and ah_from = 2";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
+
+
+    //--------------------------MARVEL-----------------------------------
+
+    private void allMarvelChars(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, sh_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Superhero, Race " +
+            "where nl_ID = sh_ID and sh_race = r_ID and sh_from = 1 " +
+            "UNION " +
+            "SELECT nl_Name as realName, v_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Villain, Race " +
+            "where nl_ID = v_ID and v_race = r_ID and v_from = 1 " +
+            "union  " +
+            "SELECT nl_Name as realName, ah_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Antihero, Race " +
+            "where nl_ID = ah_ID and ah_race = r_ID and ah_from = 1";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
+    
+    private void allMarvelHeroes(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, sh_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Superhero, Race " +
+            "where nl_ID = sh_ID and sh_race = r_ID and sh_from = 1";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
+    private void allMarvelVillains(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, v_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Villain, Race " +
+            "where nl_ID = v_ID and v_race = r_ID and v_from = 1";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
+    private void allMarvelAntiHeroes(){
+
+        try {
+            String sql = "SELECT nl_Name as realName, ah_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Antihero, Race " +
+            "where nl_ID = ah_ID and ah_race = r_ID and ah_from = 1";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
+// ----------------------- Both DC and Marvel
+    private void AllChars(){
+        try {
+            String sql = "SELECT nl_Name as realName, sh_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Superhero, Race " +
+            "where nl_ID = sh_ID and sh_race = r_ID  " +
+            "UNION " +
+            "SELECT nl_Name as realName, v_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Villain, Race " +
+            "where nl_ID = v_ID and v_race = r_ID " +
+            "union  " +
+            "SELECT nl_Name as realName, ah_Name as character, r_Name as type_race " +
+            "from NormalIdentity, Antihero, Race " +
+            "where nl_ID = ah_ID and ah_race = r_ID;";
+
+            PreparedStatement stmt = c.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            System.out.printf("%-45s %-45s %-45s\n", "Real Name", "Character Name","race");
+
+            while(rs.next()){
+                String realName = rs.getString(1);
+                String charac = rs.getString(2);
+                String race = rs.getString(3);
+                System.out.printf("%-45s%-45s%-45s\n", realName, charac,race);
+                //System.out.println(realName + " | " + charac + " | " + race);
+            }
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+
 
     
     public static void main(String args[]){
@@ -240,13 +522,154 @@ public class proj {
 
         sj.openConnection("data.sqlite");
 
-         sj.dropTables();
-         sj.createTables();
+         //sj.dropTables();
+         //sj.createTables();
         // sj.populateTables();
 
         // //queries
         // sj.queryName();
         // ...
+
+        Scanner input = new Scanner(System.in);
+
+      System.out.println("Welcome to our Marvel/DC character Database.");
+      System.out.println("Are you a new User or the Admin?");
+      System.out.println("If new User please enter 1");
+      System.out.println("If Admin please enter 2");
+      int whoareyou = input.nextInt();
+
+      if (whoareyou == 1){
+        System.out.println("Welcome new User what would you like to look at?");
+        System.out.println("the Marvel Database press 1");
+        System.out.println("The DC Database press 2");
+        System.out.println("Or both at them press 3");
+        int whatyouwannasee = input.nextInt();
+
+        if (whatyouwannasee == 1){
+          System.out.println("welcome to Marvel Database what do wanna see buddy");
+          System.out.println("To see all characters press 1");
+          System.out.println("To see a specific character press 2");
+          System.out.println("To find a custom search press 3");
+          int marvelsearch = input.nextInt();
+
+          if (marvelsearch == 1){
+            System.out.println("Now choose which tables you would like to see");
+            System.out.println("To see all Heros press 1");
+            System.out.println("To see all Villians press 2");
+            System.out.println("To see all Anti-Heros press 3");
+            System.out.println("To see all of them press 4");
+            int marvelsearch1 = input.nextInt();
+
+            if (marvelsearch1 == 1){
+                System.out.println("now printing all the heros of marvel");
+                sj.allMarvelHeroes();
+            }
+            if (marvelsearch1 == 2){
+                System.out.println("now printing all the Villians of marvel");
+                sj.allMarvelVillains();
+            }
+            if (marvelsearch1 == 3){
+              System.out.println("now printing all the Anti-Heros of marvel");
+                sj.allMarvelAntiHeroes();
+            }
+            if (marvelsearch1 == 4){
+              System.out.println("now printing all the characters of marvel");
+                sj.allMarvelChars();
+            }
+          }
+          if (marvelsearch == 2){
+            System.out.println("Now choose what alignment your character belongs too");
+            System.out.println("To see all Heros press 1");
+            System.out.println("To see all Villians press 2");
+            System.out.println("To see all Anti-Heros press 3");
+            int marvelsearch2 = input.nextInt();
+          }
+          if (marvelsearch == 3){
+            System.out.println("now tell me traveler watcha looking for");
+          }
+
+
+        }
+        if (whatyouwannasee == 2){
+          System.out.println("welcome to DC Database what do wanna see buddy");
+          System.out.println("To see all characters press 1");
+          System.out.println("To see a specific character press 2");
+          System.out.println("To find a custom search press 3");
+          int dcsearch = input.nextInt();
+
+        if (dcsearch == 1){
+          System.out.println("Now choose which tables you would like to see");
+          System.out.println("To see all Heros press 1");
+          System.out.println("To see all Villians press 2");
+          System.out.println("To see all Anti-Heros press 3");
+          System.out.println("To see all of them press 4");
+          int dcsearch1 = input.nextInt();
+
+          if (dcsearch1 == 1){
+            System.out.println("now printing all the heros of DC");
+            sj.allDCHeroes();
+
+          }
+          if (dcsearch1 == 2){
+            System.out.println("now printing all the Villians of DC");
+            sj.allDCVillains();
+          }
+          if (dcsearch1 == 3){
+            System.out.println("now printing all the Anti-Heros of DC");
+            sj.allDCAntiHeroes();
+          }
+          if (dcsearch1 == 4){
+            System.out.println("now printing all the characters of DC");
+
+            sj.allDCChars();
+          }
+        }
+        if (dcsearch == 2){
+          System.out.println("Now choose what alignment your character belongs too");
+          System.out.println("To see all Heros press 1");
+          System.out.println("To see all Villians press 2");
+          System.out.println("To see all Anti-Heros press 3");
+          int dcsearch2 = input.nextInt();
+        }
+        if (dcsearch == 3){
+          System.out.println("now tell me traveler watcha looking for");
+        }
+      }
+
+        if (whatyouwannasee == 3){
+            System.out.println("welcome to Marvel & DC Database what do wanna see buddy");
+            System.out.println("To see all characters press 1");
+            System.out.println("To see a specific character press 2");
+            System.out.println("To find a custom search press 3");
+            int marveldcsearch = input.nextInt();
+
+
+            if(marveldcsearch == 1){
+                sj.AllChars();
+            }
+
+        }
+
+
+
+
+    }
+
+
+    if (whoareyou == 2){
+        System.out.println("Welcome back Master plwes put pwassword plws");
+        int psswrd = input.nextInt();
+
+        if (psswrd == 1111){
+          System.out.println("Correct password welcome back");
+        }
+        else{
+          System.out.println("Incorrect password bzz bzz");
+        }
+
+      }
+
+
 
 
         sj.closeConnection();
@@ -265,3 +688,6 @@ Calling on terminal
 -> java -classpath ".:sqlite-jdbc-3.32.3.2.jar" proj
 
 */
+
+
+//
